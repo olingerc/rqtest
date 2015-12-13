d70a9f908a79e5b34f50d1f5973540b286db3881
Christophe Olinger <christophe@linger.eu>
2015-12-13 21:25:31 +0100

    test for job deletion in registry

--- test.py
+++ test.py
@@ -6,13 +6,32 @@ from __future__ import (absolute_import, division, print_function,
 
 import sys
 sys.path.insert(0, "/home/christophe/Documents/Projects/programming_projects/rq")
-from rqtest.function import wait_a_bit
 
+from time import sleep
+
+from rqtest.functions import wait_a_bit
 from redis import Redis
 from rq import Queue
+from rq.registry import StartedJobRegistry
 
-q = Queue(connection=Redis())
+"""
+Test enqueing a job but deleting it before it has finished. The reason is to Test
+that the job is correctly remove from the redis StartedJobRegistry
+"""
 
+conn = Redis('localhost', 6379)
 
-result = q.enqueue(wait_a_bit, 3)
-print(result)
+q = Queue(connection=conn)
+job = q.enqueue(wait_a_bit, 10)
+registry = StartedJobRegistry(job.origin, conn)
+
+print('Enqueued', job)
+sleep(2)
+
+print('StartedJobRegistry, before delete:')
+print(registry.get_job_ids())
+
+job.delete()
+
+print('StartedJobRegistry, after delete:')
+print(registry.get_job_ids())
